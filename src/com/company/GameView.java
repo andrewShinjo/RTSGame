42019faebd1f672a261a177b8ee5e0acecc3d60f
Andrew Shinjo <shinjo1233@gmail.com>
2020-06-30 20:15:56 -0700

    Adding JScrollPane

--- src/com/company/GameView.java
+++ src/com/company/GameView.java
@@ -2,27 +2,41 @@ package com.company;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.geom.AffineTransform;
 
 public class GameView extends JPanel {
     private int column;
     private int row;
     private static final int size = 50;
 
+    private boolean zoomer;
+    private double zoomFactor;
+
+    public GameView() {
+
+    }
+
     @Override
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
+
         g.setColor(Color.BLACK);
         g.fillRect(0, 0, getWidth(), getHeight());
         row = (getHeight()/size);
         column = getWidth()/size;
 
         g.setColor(Color.white);
-        for (int i = 0; i < row; i++){
-            g.drawLine(0,size+i*size, size*column,size+i*size);
+        for(int i = 0; i < row; i++) {
+            for(int j = 0; j < column; j++) {
+                g.drawRect(i * size, j * size, size, size);
+            }
         }
-
-        for(int j = 0; j< column;j++){
-            g.drawLine(size+j*size,0,size+j*size,size*row);
-        }
+//        for (int i = 0; i < row; i++){
+//            g.drawLine(0,size+i*size, size*column,size+i*size);
+//        }
+//
+//        for(int j = 0; j< column;j++){
+//            g.drawLine(size+j*size,0,size+j*size,size*row);
+//        }
     }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,6 +7,7 @@ public class Main {
     public static void main(String[] args) {
         JFrame f = new JFrame("RTS Game");
         GameView gv = new GameView();
+
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         f.getContentPane().add(gv);
         f.setExtendedState(JFrame.MAXIMIZED_BOTH);
